fbb4ab8ee0cb215b348fff1df528625764327ff2
林宏宇 <k86gundam@gmail.com>
2019-11-17 17:22:10 +0800

    加上KendoUI GridView

--- KendoUIWeb/Controllers/HomeController.cs
+++ KendoUIWeb/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using KendoUIWeb.Models;
 
 namespace KendoUIWeb.Controllers
 {
@@ -13,5 +17,26 @@ namespace KendoUIWeb.Controllers
         {
             return View();
         }
+
+        #region Ajax資料處理
+
+        public JsonResult Get_Books([DataSourceRequest] DataSourceRequest request)
+        {
+            List<Book> data = new List<Book>();
+            var dataCount = 25;
+
+            for (int i = 1; i <= dataCount; i++)
+            {
+                var book = new Book();
+                book.BookId = i;
+                book.BookName = $"TEST-{i}";
+                book.BookPrice = i * 10;
+                data.Add(book);
+            }
+
+            return Json(data.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion Ajax資料處理
     }
 }
---
+++ KendoUIWeb/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoUIWeb.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public decimal BookPrice { get; set; }
+    }
+}
